6f17f33debf6760df36b238abc5de49d16506b1a
greengrass17 <anhvudao17@gmail.com>
2020-11-02 13:49:49 +0000

    Deploying to gh-pages from  @ 6f1954dc74eea1bea2212fc8bc66896c1f0bbaaa 🚀

--- _dist_/components/SalonInfo.js
+++ _dist_/components/SalonInfo.js
@@ -9,6 +9,8 @@ import PhoneIcon2 from "./icons/PhoneIcon.js";
 import GlobeIcon2 from "./icons/GlobeIcon.js";
 import Divider2 from "../../web_modules/@material-ui/core/Divider.js";
 import {withStyles} from "../../web_modules/@material-ui/core/styles.js";
+import DownIcon2 from "./icons/DownIcon.js";
+import Typography2 from "../../web_modules/@material-ui/core/Typography.js";
 const StyledList = withStyles((theme) => ({
   root: {
     paddingTop: theme.spacing(2)
@@ -20,6 +22,11 @@ const SalonListIcon = withStyles((theme) => ({
     paddingRight: theme.spacing(2)
   }
 }))(ListItemIcon2);
+const StyledDownIcon = withStyles((theme) => ({
+  root: {
+    marginLeft: theme.spacing(2)
+  }
+}))(DownIcon2);
 export default function SalonInfo({
   address,
   postal,
@@ -31,31 +38,39 @@ export default function SalonInfo({
   const info = {
     address: {
       icon: /* @__PURE__ */ React.createElement(PinIcon2, null),
-      text: `${address}, ${postal}`
+      text: `${address}, ${postal}`,
+      expandable: false
     },
     hour: {
       icon: /* @__PURE__ */ React.createElement(ClockIcon2, null),
-      text: `\xD6ppet till ${openUntil} idag`
+      text: `\xD6ppet till ${openUntil} idag`,
+      expandable: true
     },
     phone: {
       icon: /* @__PURE__ */ React.createElement(PhoneIcon2, null),
-      text: phone
+      text: phone,
+      expandable: false
     },
     website: {
       icon: /* @__PURE__ */ React.createElement(GlobeIcon2, null),
-      text: website
+      text: website,
+      expandable: false
     },
     description: {
       icon: null,
-      text: description
+      text: description,
+      expandable: false
     }
   };
-  return /* @__PURE__ */ React.createElement(StyledList, null, Object.entries(info).map(([key, {icon, text}], index, entries) => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(ListItem2, {
+  return /* @__PURE__ */ React.createElement(StyledList, null, Object.entries(info).map(([key, {icon, text, expandable}], index, entries) => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(ListItem2, {
     key
   }, icon && /* @__PURE__ */ React.createElement(SalonListIcon, null, icon), /* @__PURE__ */ React.createElement(ListItemText2, {
-    primary: text,
-    primaryTypographyProps: {
-      variant: "body2"
-    }
-  })), index < entries.length - 1 && /* @__PURE__ */ React.createElement(Divider2, null))));
+    disableTypography: true
+  }, /* @__PURE__ */ React.createElement(Typography2, {
+    variant: "body2",
+    display: "inline"
+  }, text), expandable && /* @__PURE__ */ React.createElement(StyledDownIcon, {
+    color: "secondary",
+    fontSize: "small"
+  }))), index < entries.length - 1 && /* @__PURE__ */ React.createElement(Divider2, null))));
 }
